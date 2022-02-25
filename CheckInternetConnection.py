d6c29165f40b8179ffa39aa2e9e3064ffd1d4f18
chatchai.ti <chatchai.ti@hengleasing.co.th>
2022-02-25 16:26:20 +0700

    use env

--- CheckInternetConnection.py
+++ CheckInternetConnection.py
@@ -14,9 +14,54 @@ load_dotenv()
 line_token = os.getenv('LINE_TOKEN')
 user_name = os.getenv('USER_NAME')
 pass_word = os.getenv('PASS_WORD')
-chexk_time = True
+second_check = os.getenv('SECOND_CHECK')
+com_name = os.getenv('COMPUTERNAME')
+mode = os.getenv('MODE')
+check_time = bool(os.getenv('CHECK_TIME'))
+begin_time = os.getenv('BEGIN_TIME')
+end_time = os.getenv('END_TIME')
 
-print(line_token)
+is_send_noti = True
+disconnect_date = datetime.datetime.now()
+
+print(f"LINE_TOKEN : {line_token}")
+print(f"SECOND_CHECK : {second_check}")
+print(f"COMPUTERNAME : {com_name}")
+print(f"MODE : {mode}")
+print(f"CHECK_TIME : {check_time}")
+print(f"BEGIN_TIME : {begin_time}")
+print(f"END_TIME : {end_time}")
+
+def run():
+    while True:
+        time.sleep(int(second_check))
+        now = datetime.datetime.now()
+        print(now.strftime("%X"))
+        if check_time:
+            if now.strftime("%X") < begin_time or now.strftime("%X") > end_time:
+                continue
+
+        isConnected =  check_internet()
+        print(f"loop => {isConnected}")
+        if isConnected == False:
+            if mode == "Reconnect_CCA":
+                reconnect_internet()
+
+            global is_send_noti
+            global disconnect_date
+            if(is_send_noti == True):
+                is_send_noti = False
+                disconnect_date = datetime.datetime.now()
+
+        text = f"Computer : {com_name} \n Disconnected when :({disconnect_date}) \n Reconnected => Sucesssed : ({datetime.datetime.now()})"
+        check_line_noti(text,isConnected)
+
+def check_line_noti(text,isConnected):
+    global is_send_noti
+    if is_send_noti == False and isConnected == True:
+        line_noti(line_token,text)
+        is_send_noti = True
+    return
 
 def check_internet():
     url='https://www.google.co.th/'
@@ -53,30 +98,12 @@ def reconnect_internet():
         return
 
 def line_noti(token, message):
-    url = 'https://notify-api.line.me/api/notify'
-    headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+    try:
+        url = 'https://notify-api.line.me/api/notify'
+        headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
 
-    req = requests.post(url, headers=headers, data = {'message':message})
-    #print(req.text)
-    return
+        req = requests.post(url, headers=headers, data = {'message':message})
+    except:
+        return
 
-while True:
-    time.sleep(5)
-    now = datetime.datetime.now()
-    print(now.strftime("%X"))
-    if chexk_time:
-        if now.strftime("%X") < "04:00:00" or now.strftime("%X") > "10:30:00":
-            continue
-
-    isConnected =  check_internet()
-    print(f"loop => {isConnected}")
-    if isConnected == True:
-        continue
-
-    reconnect_internet()
-    isConnected =  check_internet()
-    line_noti(line_token,f" Reconnect Internet => Sucesssed : [{isConnected}] at ({now})")
-
-
-
-#pyinstaller
+run()
